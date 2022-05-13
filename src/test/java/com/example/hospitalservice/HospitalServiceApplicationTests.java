b16db1423eb7ac5254e3569cc272d716e5286e43
Alex Igbokwe <juvitafrica@gmail.com>
2022-05-13 15:48:44 +0100

    Clean up

--- src/test/java/com/example/hospitalservice/HospitalServiceApplicationTests.java
+++ src/test/java/com/example/hospitalservice/HospitalServiceApplicationTests.java
@@ -5,13 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import com.example.hospitalservice.dto.StaffDto;
 import com.example.hospitalservice.entities.Staff;
 import com.example.hospitalservice.services.StaffService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
